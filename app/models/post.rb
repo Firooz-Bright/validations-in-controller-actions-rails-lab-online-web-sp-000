654e234b9f52de5a8f2bbbedd48a34549e059c53
Firooz Mafakheri <firooz.mafakheri@gmail.com>
2021-04-02 00:29:12 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   validates :title, :category, :content, presence:true 
-  validates :title, uniqueness: true 
+  validates :title, :category, uniqueness: true 
   validates :content, length: {minimum: 100}
-  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+ # validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
 end
